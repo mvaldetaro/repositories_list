af6d791d3dcebd4eb9ce8ea487b196eb8ad33867
Magno Valdetaro <mvaldetaro@gmail.com>
2020-03-06 08:04:34 -0300

    Estilazando a view main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
     <>
-      <Title error>
-        Main<small>Subtitle</small>
-      </Title>
-      <Title>
-        Main<small>Subtitle</small>
-      </Title>
+      <Container>
+        <h1>
+          <FaGithubAlt /> Repositórios
+        </h1>
+
+        <Form onSubmit={() => {}}>
+          <input type="text" placeholder="Adicionar repositório" />
+          <SubmitButton>
+            <FaPlus color="#FFF" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
     </>
   );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,49 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: ${props => (props.error ? 'red' : '#7159c1')};
-  font-family: Arial, Helvetica, sans-serif;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
 
-  small {
-    font-size: 14px;
-    color: #333;
+  h1 {
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+
+    svg {
+      margin-right: 8px;
+    }
   }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #5d7bcf;
+    color: #242f4f;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({ type: 'submit' })`
+  background: #5d7bcf;
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+  color: #fff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
